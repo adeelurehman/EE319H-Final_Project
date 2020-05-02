1bf10c0e10e637f2be52d03c0c9f7c10ff51ad62
Zachary Bouthillette <zachrbou@gmail.com>
2020-05-01 22:20:21 -0500

    i think this collision actually works

--- Player.cpp
+++ Player.cpp
@@ -63,8 +63,20 @@ void Player::update() {
 	}
 	if(fail == 1)
 	{
-		Ypos = levelOneB[progB].y-height;
-		Yvel = 0;
+		if(this->Ypos+this->height > levelOneB[progB].y)
+		{
+			Ypos = levelOneB[progB].y-height;
+			Yvel = 0;
+			Xpos += Xvel;
+		}
+		else
+		{
+			Xpos = levelOneB[progB].x-width;
+			Xvel = 0;
+			Ypos += Yvel;
+			Yvel += Y_GRAVITY;
+		}
+			
 	}
 	else{
 		Xpos += Xvel;
